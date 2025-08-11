9dfb9862e4528139748656fec7ff9405108ce26b
0374flop <mikola.golovach10@gmail.com>
2025-08-11 05:16:10 +0300

    Refactor logger to remove file logging functionality. The logger now only outputs messages to the console, simplifying the logging process.

--- src/logger.js
+++ src/logger.js
@@ -1,18 +1,7 @@
-const fs = require('fs');
-const path = require('path');
-
-const LOG_FILE = path.join(__dirname, '..', 'data', 'logs.txt');
-
 function log(message, level = 'info') {
     const timestamp = new Date().toISOString();
     const logMessage = `[${level.toUpperCase()}] ${message}`;
-    const fileLogMessage = `[${timestamp}] [${level.toUpperCase()}] ${message}`;
     console.log(logMessage);
-    fs.appendFile(LOG_FILE, fileLogMessage + '\n', err => {
-        if (err) {
-            console.error('Logger error:', err);
-        }
-    });
 }
 
 function getLogger(moduleName) {
