5ae6a669599dd2aa13537b23f615dc91f70959c2
0374flop <mikola.golovach10@gmail.com>
2025-08-11 05:51:51 +0300

    Refactor WebSocket connection handling in client.js. Introduced a connectWebSocket function to manage reconnections and improved logging for connection status and errors.

--- client.js
+++ client.js
@@ -1,32 +1,39 @@
 const WebSocket = require('ws');
-const { evalinsandbox, isValidCode, isJSON } = require('./index')
+const { evalinsandbox, isValidCode, isJSON } = require('./index');
 
-const ws = new WebSocket('ws://localhost:9374');
+function connectWebSocket() {
+    const ws = new WebSocket('ws://localhost:9374')
 
-ws.on('open', () => {
-    console.log('Подключено');
-});
+    ws.on('open', () => {
+        console.log('Подключено.');
+    });
 
-ws.on('message', (message) => {
-    if (isValidCode(message)) evalinsandbox(message, ws);
-    if (isJSON(message)) {
-        const data = JSON.parse(message);
-        const main_command = data.M_command;
-        if (main_command === 'exit') {
-            ws.send('exit code 0.')
-            ws.close(1000);
-            process.exit(0);
-        } else if (main_command === 'disconnect') {
-            ws.send('close.')
-            ws.close(1000);
+    ws.on('message', (message) => {
+        if (isValidCode(message)) evalinsandbox(message, ws);
+        if (isJSON(message)) {
+            const data = JSON.parse(message);
+            const main_command = data.M_command;
+            if (main_command === 'exit') {
+                ws.send('exit code 0.');
+                ws.close(1000);
+                process.exit(0);
+            } else if (main_command === 'disconnect') {
+                ws.send('close.');
+                ws.close(1000);
+            }
         }
-    }
-});
+    });
 
-ws.on('close', () => {
-    console.log('Соединение закрыто');
-});
+    ws.on('close', () => {
+        const timereconnect = 60000;
+        console.log('Соединение закрыто.');
+        console.log('переподключение через ' + timereconnect + 'ms.');
+        setTimeout(connectWebSocket, timereconnect);
+    });
 
-ws.on('error', (error) => {
-    console.error('Ошибка:', error);
-});
+    ws.on('error', (error) => {
+        ws.close();
+    });
+}
+
+connectWebSocket();
