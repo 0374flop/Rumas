12acce07a9626b049f9748b7aea2d54a615b7e39
0374flop <mikola.golovach10@gmail.com>
2026-01-06 14:15:19 +0200

    вернул норм кал

--- newrumas/testClient.js
+++ newrumas/testClient.js
@@ -1,6 +1,6 @@
 const { Rumas } = require('./index');
 
-Rumas.Client.connect('ws://localhost:1243'); // kit-touched-commonly.ngrok-free.app
+Rumas.Client.connect('wss://kit-touched-commonly.ngrok-free.app'); // localhost:1243
 
 process.on('SIGINT', () => {
     try {Rumas.Client.vmoff();} catch {}
